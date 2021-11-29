e67a5fffff1aebbd46b0b74878baaf41bf99d210
Lasha Maruashvili <38511844+maruashvililasha@users.noreply.github.com>
2021-11-29 18:59:21 +0400

    Initial Commit

---
+++ Processica Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Processica Movies
+//
+//  Created by Lasha Maruashvili on 29.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
